6704684c69cde5be3a460b27149c8c5a1a864385
HarshitPeriwal24 <hperiwal_be24@thapar.edu>
2026-01-19 16:45:19 +0530

    Update q4.cpp

--- DAA Lab Assignment/Lab assignment 1/q4.cpp
+++ DAA Lab Assignment/Lab assignment 1/q4.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-int maxSubarraySum(int arr[], int n)
-{
-    int maxSum = arr[0];
-    int currentSum = arr[0];
+int maxCrossingSum(int arr[], int l, int m, int h) {
+    int sum = 0;
+    int left_sum = INT_MIN;
 
-    for (int i = 1; i < n; i++)
-    {
-        currentSum = max(arr[i], currentSum + arr[i]);
-        maxSum = max(maxSum, currentSum);
+    for (int i = m; i >= l; i--) {
+        sum += arr[i];
+        if (sum > left_sum)
+            left_sum = sum;
     }
 
-    return maxSum;
+    sum = 0;
+    int right_sum = INT_MIN;
+    for (int i = m + 1; i <= h; i++) {
+        sum += arr[i];
+        if (sum > right_sum)
+            right_sum = sum;
+    }
+
+    return left_sum + right_sum;
 }
 
-int main()
-{
+int maxSubArraySum(int arr[], int l, int h) {
+    if (l == h)
+        return arr[l];
+
+    int m = (l + h) / 2;
+
+    return max(max(maxSubArraySum(arr, l, m),
+                   maxSubArraySum(arr, m + 1, h)),
+               maxCrossingSum(arr, l, m, h));
+}
+
+int main() {
     int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Maximum Subarray Sum = " << maxSubarraySum(arr, n);
-
+    cout << "Maximum subarray sum is "
+         << maxSubArraySum(arr, 0, n - 1);
     return 0;
 }
